e9aabdccdd511d0dfd228956719e636a520b332d
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2026-05-18 19:30:37 +0300

    08

--- 08/ex02/src/MutantStack.hpp
+++ 08/ex02/src/MutantStack.hpp
@@ -1,24 +1,44 @@
 #pragma once
 
-#include <iterator>
 #include <stack>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
-private:
-	std::stack<T> _stack;
-
 public:
+	using container = typename std::stack<T>::container_type;
+
+	using iterator = typename container::iterator;
+	using reverse_iterator = typename container::reverse_iterator;
+	using const_iterator = typename container::const_iterator;
+	using const_reverse_iterator = typename container::const_reverse_iterator;
+
+	MutantStack() = default;
+	MutantStack(const MutantStack& other) = default;
+	~MutantStack() = default;
+	MutantStack& operator=(const MutantStack& other) = default;
+
 	auto begin() {
 		return this->c.begin();
 	}
 	auto end() {
 		return this->c.end();
 	}
-	auto cbegin() {
+	auto rbegin() {
+		return this->c.rbegin();
+	}
+	auto rend() {
+		return this->c.rend();
+	}
+	auto cbegin() const {
 		return this->c.cbegin();
 	}
-	auto cend() {
+	auto cend() const {
 		return this->c.cend();
+	}
+	auto crbegin() const {
+		return this->c.crbegin();
+	}
+	auto crend() const {
+		return this->c.crend();
 	}
 };
--- 08/ex02/src/main.cpp
+++ 08/ex02/src/main.cpp
@@ -1,64 +1,81 @@
-#include <array>
-#include <concepts>
-#include <deque>
-#include <forward_list>
 #include <iostream>
 #include <list>
-#include <ranges>
+#include <stack>
 #include <string_view>
-#include <vector>
 
-#include "easyfind.hpp"
-
-#define TEST(container, value) test(container, value, #container)
-
-template <std::ranges::range T>
-	requires std::same_as<std::ranges::range_value_t<T>, int>
-void test(T& container, int value, std::string_view name) {
-	name.remove_suffix(1);
-	std::cout << name << " contains " << value << ": ";
-	std::cout << (easyfind(container, value) != std::ranges::end(container)) << '\n';
-}
+#include "MutantStack.hpp"
 
 int main() {
-	std::cout << std::boolalpha;
+	{
+		std::cout << "Subject test for MutantStack.\n";
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::endl;
 
-	std::array<int, 3> array_{ 1, 2, 3 };
-	TEST(array_, 1);
-	TEST(array_, 0);
-	std::cout << '\n';
+	{
+		std::cout << "Same but list instead of MutantStack.\n";
+		std::list<int> list_;
+		list_.push_back(5);
+		list_.push_back(17);
+		std::cout << list_.back() << std::endl;
+		list_.pop_back();
+		std::cout << list_.size() << std::endl;
+		list_.push_back(3);
+		list_.push_back(5);
+		list_.push_back(737);
+		//[...]
+		list_.push_back(0);
+		auto it = list_.begin();
+		auto ite = list_.end();
+		++it;
+		--it;
+		while (it != ite) {
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	std::cout << std::endl;
 
-	std::list<int> list_{ 1, 2, 3 };
-	TEST(list_, 1);
-	TEST(list_, 0);
-	std::cout << '\n';
-
-	std::forward_list<int> forward_list_{ 1, 2, 3 };
-	TEST(forward_list_, 1);
-	TEST(forward_list_, 0);
-	std::cout << '\n';
-
-	std::deque<int> deque_{ 1, 2, 3 };
-	TEST(deque_, 1);
-	TEST(deque_, 0);
-	std::cout << '\n';
-
-	std::vector<int> vector_{ 1, 2, 3 };
-	TEST(vector_, 1);
-	TEST(vector_, 0);
-	std::cout << '\n';
-
-	int c_array_[3]{ 1, 2, 3 };
-	TEST(c_array_, 1);
-	TEST(c_array_, 0);
-
-	// Doesn't compile due to required concepts
-
-	// std::array<float, 3> floatArray_{ 1, 2, 3 };
-	// TEST(floatArray_, 1);
-
-	// int int_ = 1;
-	// TEST(int_, 1);
-
+	{
+		std::cout << "Testing MutantStack with string_view and constant iterators.\n";
+		MutantStack<std::string_view> mstack;
+		mstack.push("1");
+		mstack.push("2");
+		mstack.push("3");
+		mstack.push("Sauna!");
+		std::cout << "Forward\n";
+		auto cit = mstack.cbegin();
+		auto cend = mstack.cend();
+		while (cit != cend) {
+			std::cout << *cit << '\n';
+			++cit;
+		}
+		std::cout << "\nBackward\n";
+		auto crit = mstack.crbegin();
+		auto crend = mstack.crend();
+		while (crit != crend) {
+			std::cout << *crit << '\n';
+			++crit;
+		}
+	}
 	return 0;
 }
